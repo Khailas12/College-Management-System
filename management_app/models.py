cc2958e44b2447ebe404a30121c42e88809f789d
Khailas R <khailas303@gmail.com>
2022-08-18 04:02:31 +0530

    intentation fix

--- management_app/models.py
+++ management_app/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
-
 
 class SessionYearModel(models.Model):
     id = models.AutoField(primary_key=True)
@@ -180,8 +178,10 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         if instance.user_type == 1:
             AdminHOD.objects.create(admin=instance)
+            
         if instance.user_type == 2:
             Staffs.objects.create(admin=instance, address="")
+            
         if instance.user_type == 3:
             Students.objects.create(admin=instance, course_id=Courses.objects.get(
                 id=1), session_year_id=SessionYearModel.object.get(id=1), address="", profile_pic="", gender="")
@@ -191,7 +191,9 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     if instance.user_type == 1:
         instance.adminhod.save()
+        
     if instance.user_type == 2:
         instance.staffs.save()
+        
     if instance.user_type == 3:
         instance.students.save()
