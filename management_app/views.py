f0b5ef3e9084ee42071352c4cf328c7019f621e7
Khailas R <khailas303@gmail.com>
2022-08-19 03:17:17 +0530

    student sign up page updated

--- management_app/views.py
+++ management_app/views.py
@@ -22,17 +22,6 @@ def doLogin(request):
         return HttpResponse("<h2>Method Not Allowed</h2>")
     
     else:
-        # captcha_token=request.POST.get("g-recaptcha-response")
-        # cap_url="https://www.google.com/recaptcha/api/siteverify"
-        # cap_secret="6LeWtqUZAAAAANlv3se4uw5WAg-p0X61CJjHPxKT"
-        # cap_data={"secret":cap_secret,"response":captcha_token}
-        # cap_server_response=requests.post(url=cap_url,data=cap_data)
-        # cap_json=json.loads(cap_server_response.text)
-
-        # if cap_json['success']==False:
-        #     messages.error(request,"Invalid Captcha Try Again")
-        #     return HttpResponseRedirect("/")
-
         user = EmailBackEnd.authenticate(request, username=request.POST.get(
             "email"), password=request.POST.get("password"))
 
@@ -158,13 +147,12 @@ def do_signup_student(request):
     session_year_id = request.POST.get("session_year")
     course_id = request.POST.get("course")
     sex = request.POST.get("sex")
-
-    profile_pic = request.FILES['profile_pic']
+    profile_pic = request.POST.get('profile_pic')
+    
     fs = FileSystemStorage()
-    filename = fs.save(profile_pic.name, profile_pic)
-    profile_pic_url = fs.url(filename)
+    # filename = fs.save(profile_pic.name, profile_pic)
+    # profile_pic_url = fs.url(filename)
 
-    # try:
     user = CustomUser.objects.create_user(
         username=username, password=password, email=email,
         last_name=last_name,  first_name=first_name, user_type=3
@@ -176,7 +164,7 @@ def do_signup_student(request):
     session_year = SessionYearModel.object.get(id=session_year_id)
     user.students.session_year_id = session_year
     user.students.gender = sex
-    user.students.profile_pic = profile_pic_url
+    # user.students.profile_pic = profile_pic_url
     user.save()
     
     messages.success(request, "Successfully Added Student")
