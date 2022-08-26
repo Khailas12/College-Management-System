52ac2cd65e18233434149dfd2038b86a532b481d
Khailas R <khailas303@gmail.com>
2022-08-26 19:46:54 +0530

    attendance view bug fix

--- student/views.py
+++ student/views.py
@@ -45,51 +45,32 @@ def student_home(request):
         
     )
 
-
-# def view_attendance(request):
-#     student_obj = Students.objects.get(admin=request.user.id)
-#     attendance_total = AttendanceReport.objects.filter(student_id=student_obj).count()
-#     attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
-#     attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
-#     course = Courses.objects.get(id=student_obj.course_id.id)
-#     subjects = Subjects.objects.filter(course_id=course).count()
-    
     
 
 def student_view_attendance(request):
-    student = Students.objects.get(admin=request.user.id)
-    course = student.course_id
-    subjects = Subjects.objects.filter(course_id=course)
+    student=Students.objects.get(admin=request.user.id)
+    course=student.course_id
+    subjects=Subjects.objects.filter(course_id=course)
     
-    context = {"subjects": subjects}
-    return render(request, "student_template/student_view_attendance.html", context)
-
+    context = {"subjects":subjects}
+    return render(request,"student_template/student_view_attendance.html",context)
 
 def student_view_attendance_post(request):
-    subject_id = request.POST.get("subject")
-    start_date = request.POST.get("start_date")
-    end_date = request.POST.get("end_date")
+    subject_id=request.POST.get("subject")
+    start_date=request.POST.get("start_date")
+    end_date=request.POST.get("end_date")
+
+    start_data_parse=datetime.datetime.strptime(start_date,"%Y-%m-%d").date()
+    end_data_parse=datetime.datetime.strptime(end_date,"%Y-%m-%d").date()
+    subject_obj=Subjects.objects.get(id=subject_id)
+    user_object=CustomUser.objects.get(id=request.user.id)
+    stud_obj=Students.objects.get(admin=user_object)
+
+    attendance=Attendance.objects.filter(attendance_date__range=(start_data_parse,end_data_parse),subject_id=subject_obj)
+    attendance_reports=AttendanceReport.objects.filter(attendance_id__in=attendance,student_id=stud_obj)
     
-    try:
-        start_data_parse = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
-        end_data_parse = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
-        subject_obj = Subjects.objects.get(id=subject_id)
-        user_object = CustomUser.objects.get(id=request.user.id)
-        stud_obj = Students.objects.get(admin=user_object)
-        
-        attendance = AttendanceReport.objects.filter(student_id=stud_obj, attendance_id__subject_id=subject_obj, attendance_id__date__range=[start_data_parse, end_data_parse])
-        
-        # attendance = Attendance.objects.filter(
-        #     attendance_date__range=(start_data_parse, end_data_parse), subject_id=subject_obj)
-        attendance_reports = AttendanceReport.objects.filter(
-            attendance_id__in=attendance, student_id=stud_obj)
-        
-        context = {"attendance_reports": attendance_reports}
-        return render(request, "student_template/student_attendance_data.html", context)
-    
-    except:
-        messages.error(request, "Invalid Entry")
-        return HttpResponseRedirect(reverse("student_view_attendance"))
+    context = {"attendance_reports": attendance_reports}    
+    return render(request,"student_template/student_attendance_data.html", context)
 
 
 def student_apply_leave(request):
